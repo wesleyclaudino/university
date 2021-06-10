2257137820931ab09e831f9db62ad99dc260f874
Wesley Claudino <claudinowesley11@gmail.com>
2021-06-09 22:32:52 -0300

    update exercise 9 of c

--- c/lists/009.c
+++ c/lists/009.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void validacao(float x, float y, float z){
+bool availableTriangle(float x, float y, float z){
+
+    bool available;
     
     if(x<y+z && y<x+z && z<x+y){
-        printf("\t Os valores digitados formam um triângulo. \n");
-        
-        if(x==y && x==z){
-            printf("\t Triângulo Equilátero.");
-        }else if(x==y || x==z || y==z){
-            printf("\t Triângulo Isósceles.");
-        }else{
-            printf("\t Triângulo Escaleno.");
-        }
+        available = true;
     }else{
-        printf("\t Tais valores não formam um triângulo.");
+        available = false;
+    }
+    
+    return available;
+}
+
+void typeTriangle(float x, float y, float z){
+
+    if(x==y && x==z){
+        printf("\t Triângulo Equilátero.");
+    }else if(x==y || x==z || y==z){
+        printf("\t Triângulo Isósceles.");
+    }else{
+        printf("\t Triângulo Escaleno.");
     }
 }
 
 int main(){
     
     float x, y, z;
+    bool available;
 
     printf("Digite os valores do triângulo: \t");
     scanf("%f %f %f", &x, &y, &z);
     
-    validacao(x, y, z);
+    available = availableTriangle(x, y, z);
+    
+    if(available == true){
+        printf("\n Os valores formam um");
+        typeTriangle(x, y, z);
+    }else{
+        printf("\n \t Os valores fornecidos não formam um triângulo.");
+    }
+    
+    return 0;
 }
