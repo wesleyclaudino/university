fcad2c7b291b795888490244f5999230baa8ad5e
Wesley Claudino <claudinowesley11@gmail.com>
2021-08-13 23:35:04 -0300

    the begin to database studies

---
+++ database/lesson01.sql
@@ -0,0 +1,16 @@
+create database cadastro;
+
+use cadastro;
+
+create table pessoas(
+nome varchar(30),
+idade tinyint,
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+
+show databases;
+show tables;
+describe pessoas;
